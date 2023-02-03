67b202fec1033d84c6ad1b1a2bbd17c27d26b3dc
Cristian Alejandro Vasquez Muñoz <123973351+cristianalejandro12@users.noreply.github.com>
2023-02-03 14:41:24 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.scroll-banner', {
+
+    duration: 3000,
+    origin: 'left',
+    distance: '-200px'
+
+
+});
+
+sr.reveal('.scroll-tarjeta', {
+
+    rotate: {
+        x : 1,
+        y: 100,
+
+    scale: 4,
+    }
+
+    
+
+});
+
+sr.reveal('.scroll-bannerr', {
+
+    duration: 3000,
+    origin: 'right',
+    distance: '-200px'
+
+
+});
